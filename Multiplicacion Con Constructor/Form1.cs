935898045400c8bbf6fc1699451f7f049f79ee78
JesusAdanOp <jesusadanorozcop@outlook.com>
2022-09-19 08:50:57 -0600

    Agregar archivos de proyecto.

---
+++ Multiplicacion Con Constructor/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multiplicacion_Con_Constructor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMult_Click(object sender, EventArgs e)
+        {
+            clmult mult = new clmult(int.Parse(txtV1.Text), int.Parse(txtV2.Text));
+            txtResult.Text = mult.multiplicacion().ToString();
+        }
+    }
+}
---
+++ Multiplicacion Con Constructor/clmult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplicacion_Con_Constructor
+{
+    internal class clmult
+    {
+        int v1, v2;
+
+        public clmult(int v1, int v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+
+        public int multiplicacion()
+        {
+            return v1 * v2;
+        }
+    }
+}
